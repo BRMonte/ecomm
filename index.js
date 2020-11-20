f05d4d6cc25209463d8a563aa3a5358f949b25e1
Bruno Montebello <brunormontebello@hotmail.com>
2020-11-20 19:22:35 +0000

    GEt method

--- index.js
+++ index.js
@@ -3,9 +3,23 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('hi, there');
+  res.send(`
+    <div>
+      <form method="POST">
+        <input name="email" placeholder="email"/>
+        <input name="password" placeholder="password"/>
+        <input name="passwordConfirmation" placeholder="password confirmaion"/>
+        <button>SIgn Up</button>
+      </form>
+    </div>
+  `);
+});
+
+app.post('/', (req, res) => {
+  res.send('Account created!!!');
 });
 
 app.listen(3000, () => {
   console.log('Listening');
 });
+

81567fea3d4c6dd9ba0cfdef170c002643e309f3
Bruno Montebello <brunormontebello@hotmail.com>
2020-11-22 20:16:10 +0000

    Create Repo

---
+++ repositories/users.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+
+class UsersRepository {
+  constructor(filename) {
+    if (!filename) {
+      throw new Error('Creating a repository requires a filename');
+    }
+    this.filename = filename;
+    try {
+      fs.accessSync(this.filename);
+    } catch (err) {
+      fs.writeFileSync(this.filename, '[]');
+    }
+  }
+
+  async getAll() {
+    // open the file called this.filename
+    const contents = await fs.promises.readFile(this.filename, { encoding: 'utf8' });
+
+    // read its content
+    console.log(contents);
+
+    // parse the contents
+
+    // return the parsed data
+  }
+}
+
+const test = async () => {
+  const repo = new UsersRepository('users.json');
+  await repo.getAll();
+};
